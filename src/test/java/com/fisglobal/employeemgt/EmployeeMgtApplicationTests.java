c669829d9d184240e57e80a11065bbe1d4c7d794
Ajay Gupta <ajaygpt28@gmail.com>
2022-04-14 22:39:12 +0530

    Starter kit

---
+++ src/test/java/com/fisglobal/employeemgt/EmployeeMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisglobal.employeemgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
